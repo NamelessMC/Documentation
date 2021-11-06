f8cdb2262752ec128a47720b5fe6cef52e439d3f
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2021-11-06 09:20:29 +0300

    b

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -28,7 +28,7 @@ module.exports = {
         ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' }],
         ['link', { rel: 'icon', type: 'image/png', href: '/favicons/favicon-32x32.png', sizes: '32x32' }],
         ['link', { rel: 'icon', type: 'image/png', href: '/favicons/favicon-16x16.png', sizes: '16x16' }],
-        ['link', { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#0e4688' }],
+        ['link', { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#c6a951' }],
         ['link', { rel: 'manifest', href: '/favicons/site.webmanifest' }],
         ['link', { rel: 'shortcut icon', href: '/favicons/favicon.ico' }],
         ['meta', { name: 'msapplication-config', content: '/favicons/browserconfig.xml' }],
@@ -43,83 +43,11 @@ module.exports = {
         logo: '/logos/nameless.svg',
         nav: [
             {
-                text: 'Documentation',
-                link: '/project/introduction.md',
-            },
-            {
-                text: 'Community Guides',
-                link: '/community/about.md',
-            },
-            {
                 text: 'Discord',
                 link: 'https://discord.gg/nameless'
             }
         ],
         sidebar: {
-            '/community/': [
-                {
-                    title: 'Community Guides',
-                    collapsable: false,
-                    children: [
-                        '/community/about.md',
-                    ]
-                },
-                {
-                    title: 'Panel Installation',
-                    collapsable: false,
-                    children: [
-                        '/community/installation-guides/panel/centos7.md',
-                        '/community/installation-guides/panel/centos8.md',
-                        '/community/installation-guides/panel/debian9.md',
-                        '/community/installation-guides/panel/debian10.md',
-                        '/community/installation-guides/panel/ubuntu1804.md',
-                        '/community/installation-guides/panel/ubuntu2004.md',
-                    ]
-                },
-                {
-                    title: 'Wings Installation',
-                    collapsable: false,
-                    children: [
-                        '/community/installation-guides/wings/centos7.md',
-                        '/community/installation-guides/wings/centos8.md',
-                        '/community/installation-guides/wings/debian9.md',
-                        '/community/installation-guides/wings/debian10.md',
-                        '/community/installation-guides/wings/ubuntu1804.md',
-                        '/community/installation-guides/wings/ubuntu2004.md',
-                    ]
-                },
-                {
-                    title: 'Creating Eggs',
-                    collapsable: false,
-                    children: [
-                        '/community/config/eggs/creating_a_custom_egg.md',
-                        '/community/config/eggs/creating_a_custom_image.md',
-                    ],
-                },
-                {
-                    title: 'Game Configuration',
-                    collapsable: false,
-                    children: [
-                        '/community/games/minecraft.md',
-                    ],
-                },
-                {
-                    title: 'Tutorials',
-                    collapsable: false,
-                    children: [
-                        '/community/config/nodes/add_node.md',
-                        '/community/tutorials/artisan.md',
-                    ],
-                },
-                {
-                    title: 'Customization',
-                    collapsable: false,
-                    children: [
-                        '/community/customization/panel.md',
-                        '/community/customization/wings.md',
-                    ],
-                },
-            ],
             '/': [
                 {
                     title: 'NamelessMC',
@@ -129,66 +57,11 @@ module.exports = {
                     ]
                 },
                 {
-                    title: 'Panel',
+                    title: 'Setup',
                     collapsable: false,
-                    path: '/panel/',
-                    currentVersion: '1.0',
-                    versions: [
-                        {
-                            title: '1.6',
-                            name: '1.0',
-                            status: 'stable',
-                            children: [
-                                '/getting_started',
-                                '/webserver_configuration',
-                                '/additional_configuration',
-                                '/updating',
-                                '/troubleshooting',
-                                '/legacy_upgrade',
-                            ]
-                        }
+                    children: [
+                        '/setup/installation.md'
                     ]
-                },
-                {
-                    title: 'Wings',
-                    collapsable: false,
-                    path: '/wings/',
-                    currentVersion: '1.0',
-                    versions: [
-                        {
-                            title: '1.5',
-                            name: '1.0',
-                            status: 'stable',
-                            children: [
-                                '/installing',
-                                '/upgrading',
-                                '/migrating',
-                                '/configuration',
-                            ]
-                        }
-                    ]
-                },
-                {
-                    title: 'Tutorials',
-                    collapsable: false,
-                    children: [
-                        '/tutorials/mysql_setup.md',
-                        '/tutorials/creating_ssl_certificates.md',
-                    ],
-                },
-                {
-                    title: 'Guides',
-                    collapsable: false,
-                    children: [
-                        '/guides/mounts.md',
-                    ],
-                },
-                {
-                    title: 'Development & Ops',
-                    collapsable: true,
-                    children: [
-                        '/ops/publish_release.md',
-                    ],
                 },
             ],
         },
---
+++ setup/installation.md
@@ -0,0 +1,98 @@
+# Getting Started
+
+[[toc]]
+## Dependencies
+* PHP `7.4` (recommended) or `8.0` with the following extensions:
+    - php-curl
+    - php-exif (optional)
+    - php-gd with png and jpeg support
+    - php-mbstring
+    - php-mysql or php-mysqlnd
+    - php-pdo
+    - php-xml
+* MySQL `5.7.22` or higher (MySQL `8` recommended) **or** MariaDB `10.2` or higher.
+* A webserver (Apache, NGINX)
+
+::: warning
+PHP 8 works for the most part but still has some issues, please stick with PHP 7.4 for now.
+:::
+
+## Installing
+Here we will be covering installing NamelessMC on Ubuntu
+### Installing dependencies
+```bash
+# Add "add-apt-repository" command
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+
+# Add additional repositories for PHP and MariaDB
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+
+# Update repositories list
+apt update
+
+# Add universe repository if you are on Ubuntu 18.04
+apt-add-repository universe
+
+# Install Dependencies
+
+# PHP 7.4 (recommended)
+apt -y install php7.4 php7.4-{curl,exif,gd,mbstring,mysql,pdo,xml} mariadb-server nginx tar git
+
+# PHP 8.0
+apt -y install php8.0 php8.0-{curl,exif,gd,mbstring,mysql,pdo,xml} mariadb-server nginx tar git
+```
+
+### Installation
+The first step in this process is to create the folder where the website will live and then move ourselves into that newly created folder. Below is an example of how to perform this operation.
+
+```bash
+mkdir -p /var/www/html
+cd /var/www/html
+```
+
+Once you have created a new directory for the website and moved into it you'll need to download the NamelessMC files. This
+is as simple as using `curl` to download our pre-packaged content. Once it is downloaded you'll need to unpack the archive
+and then set the correct permissions.
+
+```bash
+curl -Lo nameless.tar.gz https://github.com/NamelessMC/Nameless/archive/refs/tags/v2.0.0-pr13.tar.gz
+tar -xzvf nameless.tar.gz
+chmod -R 755 *
+```
+
+Now that all of the files have been downloaded we need to configure the database.
+
+::: tip Database Configuration
+You will need a database setup and a user with the correct permissions created for that database before
+continuing any further. See below to create a user and database for your website quickly. To find more detailed information
+please have a look at [Setting up MySQL](/tutorials/mysql_setup.html).
+
+```sql
+mysql -u root -p
+
+# Remember to change 'yourPassword' below to be a unique password
+CREATE USER 'nameless'@'127.0.0.1' IDENTIFIED BY 'yourPassword';
+CREATE DATABASE nameless;
+GRANT ALL PRIVILEGES ON nameless.* TO 'nameless'@'127.0.0.1' WITH GRANT OPTION;
+```
+
+:::
+
+
+
+## Video Tutorials
+Here's some video tutorials on installing NamelessMC
+
+**v1**
+
+* https://www.youtube.com/watch?v=aTcZ8MbBixs (v1.0.15 only, english)
+
+
+**v2**
+
+* https://www.youtube.com/watch?v=vWQM48a1qQ4 (v2-pre6 / v2-pre7, english)
+
+* https://www.youtube.com/watch?v=Lu2Bt9AKujo (v2-pre7 in depth, english)
+
+* https://www.youtube.com/watch?v=UlJTeYFHzA8 (v2-pre7, spanish)
