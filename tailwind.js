a6f8aa1a1281fd504644d37bd154b611e5018629
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2021-11-06 10:28:13 +0300

    Update tailwind.js

--- tailwind.js
+++ tailwind.js
@@ -41,12 +41,12 @@ let colors = {
     'transparent': 'transparent',
 
     'black': '#22292f',
-    'grey-darkest': '#202020',
-    'grey-darker': '#202020',
-    'grey-dark': '#202020',
-    'grey': '#202020',
-    'grey-light': '#202020',
-    'grey-lighter': '#202020',
+    'grey-darkest': '#3d4852',
+    'grey-darker': '#606f7b',
+    'grey-dark': '#8795a1',
+    'grey': '#b8c2cc',
+    'grey-light': '#dae1e7',
+    'grey-lighter': '#f1f5f8',
     'grey-lightest': '#f8fafc',
     'white': '#ffffff',
 
