eb372070f7467dc11b9cffb63d4cb5d27de12248
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2021-11-07 08:04:23 +0300

    colors yay

--- tailwind.js
+++ tailwind.js
@@ -40,7 +40,7 @@ View the full documentation at https://tailwindcss.com.
 let colors = {
     'transparent': 'transparent',
 
-    'black': '#22292f',
+    'black': '#202020',
     'grey-darkest': '#3d4852',
     'grey-darker': '#606f7b',
     'grey-dark': '#8795a1',
