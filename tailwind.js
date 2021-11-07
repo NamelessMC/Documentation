428c69ec15f07896e1d8044b7c8000278d264d56
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2021-11-07 10:13:34 +0300

    revert

--- tailwind.js
+++ tailwind.js
@@ -61,7 +61,7 @@ let colors = {
     'orange-darkest': '#462a16',
     'orange-darker': '#613b1f',
     'orange-dark': '#de751f',
-    'orange': '#f6993f',
+    'orange': '#c6a951',
     'orange-light': '#faad63',
     'orange-lighter': '#fcd9b6',
     'orange-lightest': '#fff5eb',
