bf127c69beb35c3a14b4ffb9b433be33be3686bc
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2021-12-28 11:21:23 +0300

    remove useless parts

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,39 +1,9 @@
-export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData, // site metadata
-  isServer // is this enhancement applied in server-rendering or client
-}) => {
-  findVersionedPaths(siteData.themeConfig.sidebar).forEach(vp => {
-    router.addRoutes(
-      router.options.routes.map(route => {
-        if (route.path.startsWith(vp.path + vp.currentVersion)) {
-          return [
-            {
-              path: route.path.replace(vp.currentVersion, "current"),
-              redirect: route.path
-            }, {
-              path: route.path.replace(vp.currentVersion + "/", ""),
-              redirect: route.path
-            }
-          ]
-        }
-        return undefined
-      }).filter(x => x).flat().concat([
-        { path: '/panel/1.0/upgrade/1.0.html', redirect: '/panel/1.0/updating.html' },
-        { path: '/panel/1.0/upgrade/0.7_to_1.0.html', redirect: '/panel/1.0/legacy_upgrade.html' },
-      ])
-    )
-  })
-}
-
 function findVersionedPaths(paths) {
   return Object.entries(paths).map(([path, children]) => {
     return children
       .filter(child => Array.isArray(child.versions))
       .map(child => ({ ...child, path: pathJoin(path, child.path) }))
-  }).flat()
+  }).flat();
 }
 
 // https://stackoverflow.com/a/29855282/4430124
